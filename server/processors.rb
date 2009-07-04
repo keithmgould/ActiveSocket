369cc38c0d3731b9b6e6d2465b256bbc27572064
keith@dailymugshot.com <keith@dailymugshot.com>
2009-07-04 19:41:45 -0400

    fixed typo (ok, bug) in reveal_structure

--- server/processors.rb
+++ server/processors.rb
@@ -170,8 +170,8 @@ module ActiveSocket
         
         # Now take out instance methods associated with columns
         # as those methods should be local to the client
-        c.columns.each do |c|
-          meths[:inst] = meths[:inst] - ["#{c[:name]}","#{c[:name]}=" ]
+        c.columns.each do |s|
+          meths[:inst] = meths[:inst] - ["#{s[:name]}","#{s[:name]}=" ]
         end
         
         # We don't want to include the actual ConnectionAdapter.  
